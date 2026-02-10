79c56e9eb0069ecb7c734c0123465475e5c8fde0
jeewon514 <jeewon514@gmail.com>
2026-02-11 01:16:35 +0900

    fix: setting 수정

--- src/pages/SettingPage.jsx
+++ src/pages/SettingPage.jsx
@@ -2,24 +2,35 @@
 import '../styles/Setting.css';
 
 export default function Setting() {
+  const userData = {
+    email: 'ewha2026@ewhain.com',
+  };
+
   return (
     <div className="setting-page">
       <main className="setting-main">
         <section className="profile">
           <div className="profile-img">{/* img icon */}</div>
-          <span className="profile-email">ewha2026@ewhain.com</span>
+          <span className="profile-email">{userData.email}</span>
         </section>
 
         <section className="menu">
           <button className="menu-item">프로필 편집</button>
-          <button className="menu-item">로그아웃</button>
+          <button className="menu-item" onClick={() => console.log('Logout')}>
+            로그아웃
+          </button>
         </section>
 
         <hr className="divider" />
 
         <section className="menu">
           <button className="menu-item">데이터 초기화</button>
-          <button className="menu-item">계정 삭제</button>
+          <button
+            className="menu-item"
+            onClick={() => console.log('Delete Account')}
+          >
+            계정 삭제
+          </button>
         </section>
 
         <hr className="divider" />
