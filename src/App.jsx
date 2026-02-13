f9cd0dff9afa120015f3e584e6ebf27724094c2d
jeewon514 <jeewon514@gmail.com>
2026-02-13 15:24:25 +0900

    feat: update page 추가

--- src/App.jsx
+++ src/App.jsx
@@ -1,50 +1,58 @@
+import React from 'react';
+import { BrowserRouter, Routes, Route, useNavigate } from 'react-router-dom';
 import './App.css';
+
 import Button from './components/Button';
 
-import DiscoveryIcon from './assets/Discovery.svg';
-import UpdateIcon from './assets/Update.svg';
-import FriendsIcon from './assets/Friends.svg';
-import SettingsIcon from './assets/Settings.svg';
-
-import Setting from './pages/SettingPage';
 import Login from './pages/LoginPage';
-import { BrowserRouter, Routes, Route, useNavigate } from 'react-router-dom';
+import Update from './pages/UpdatePage';
+import Setting from './pages/SettingPage';
+
+// --- 아이콘 (react-icons 사용) ---
+// FaCompass(나침반/홈), FaSyncAlt(업데이트/새로고침), FaUserFriends(친구), FaCog(설정)
+import { FaCompass, FaSyncAlt, FaUserFriends, FaCog } from 'react-icons/fa';
 
 function AppLayout() {
   const navigate = useNavigate();
 
   return (
     <div className="app-container">
+      <div className="content-container">
+        <Routes>
+          <Route path="/" element={<Login />} />
+          {/*<Route path="/home" element={<Home />} />*/}
+          <Route path="/update" element={<Update />} />
+          {/*<Route path="/friends" element={<Friends />} />*/}
+          <Route path="/setting" element={<Setting />} />
+        </Routes>
+      </div>
+
       <nav className="nav-bar">
         <Button
           label="HOME"
-          icon={DiscoveryIcon}
+          icon={<FaCompass />}
           onClick={() => navigate('/home')}
+          isActive={location.pathname === '/home'}
         />
         <Button
           label="UPDATE"
-          icon={UpdateIcon}
+          icon={<FaSyncAlt />}
           onClick={() => navigate('/update')}
+          isActive={location.pathname === '/update'}
         />
         <Button
           label="FRIENDS"
-          icon={FriendsIcon}
+          icon={<FaUserFriends />}
           onClick={() => navigate('/friends')}
+          isActive={location.pathname === '/friends'}
         />
         <Button
           label="SETTINGS"
-          icon={SettingsIcon}
+          icon={<FaCog />}
           onClick={() => navigate('/setting')}
+          isActive={location.pathname === '/setting'}
         />
       </nav>
-
-      <Routes>
-        <Route path="/" element={<Login />} />
-        {/*<Route path="/home" element={<Home />} />*/}
-        {/*<Route path="/update" element={<Update />} />*/}
-        {/*<Route path="/friends" element={<Friends />} />*/}
-        <Route path="/setting" element={<Setting />} />
-      </Routes>
     </div>
   );
 }
