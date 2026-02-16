b1ca36a4373475f41c174747ef583dfd1d567ab0
jeewon514 <jeewon514@gmail.com>
2026-02-16 20:28:54 +0900

    fix: setting.js

--- src/api/setting.js
+++ src/api/setting.js
@@ -2,7 +2,7 @@ import axios from './axios';
 
 export const getUserInfo = async () => axios.get('/users/me'); // 내 계정 정보 조회(이메일)
 
-export const ChangeName = async () => axios.patch('/users/me/nickname'); // 프로필 편집(1)
+export const ChangeName = (data) => axios.patch('/users/me/nickname', data); // 프로필 편집(1)
 export const settingLogout = async () => axios.post('/auth/logout'); // 로그아웃(2)
 export const settingDeleteData = async () => axios.delete('/settings/data'); // 데이터 초기화(3)
 export const DeleteAccount = async () => axios.delete('/users/me'); // 계정 삭제(4)
