4c478360f435e4c5b42a3d3c0c88897a1cb18e9e
jeewon514 <jeewon514@gmail.com>
2026-02-18 12:09:24 +0900

    fix: burden으로 변경(api)

--- src/pages/UpdatePage.jsx
+++ src/pages/UpdatePage.jsx
@@ -20,7 +20,7 @@ export default function UpdatePage() {
   const [userData, setUserData] = useState(null);
   const [statusData, setStatusData] = useState({
     energy: 0,
-    pressure: 0,
+    burden: 0,
     passion: 0,
   }); // getTodayStatus
   const [character, setCharacter] = useState({ summary: '', score: 0 }); // characterStatus, characterSummary
@@ -55,8 +55,10 @@ export default function UpdatePage() {
             updateApi.characterScore(),
           ]);
 
-        setUserData(userRes.data);
-        setStatusData(statusRes.data || { energy: 0, pressure: 0, passion: 0 });
+        const user = userRes.data?.data ?? userRes.data;
+        setUserData(user);
+
+        setStatusData(statusRes.data || { energy: 0, burden: 0, passion: 0 });
         // 캐릭터 상태와 요약 문장을 합쳐서 저장
         setCharacter({
           ...charRes.data,
@@ -156,23 +158,23 @@ export default function UpdatePage() {
               </div>
             </div>
 
-            <div className="stat-item pressure">
+            <div className="stat-item burden">
               <div className="label-row">
                 <FaWeightHanging className="icon" />
                 <span className="name">PRESSURE</span>
-                <span className="value">{statusData.pressure}%</span>
+                <span className="value">{statusData.burden}%</span>
               </div>
               <div className="bar-bg">
                 <div
                   className="bar-fill"
-                  style={{ width: `${statusData.pressure}%` }}
+                  style={{ width: `${statusData.burden}%` }}
                 ></div>
                 <input
                   type="range"
                   name="pressure"
                   min="0"
                   max="100"
-                  value={statusData.pressure}
+                  value={statusData.burden}
                   onChange={handleSliderChange}
                   className="real-slider"
                 ></input>
