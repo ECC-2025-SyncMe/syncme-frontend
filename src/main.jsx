b227fbf2cfbf927ae11a64c5cecfc71ed4c26c31
jeewon514 <jeewon514@gmail.com>
2026-02-18 16:09:14 +0900

    fix: google client

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import { GoogleOAuthProvider } from '@react-oauth/google';
 
-const GOOGLE_CLIENT_ID = "";
+const GOOGLE_CLIENT_ID = import.meta.env.VITE_GOOGLE_CLIENT_ID;
 
 //ReactDOM.createRoot(document.getElementById('root')).render(
 //  <GoogleOAuthProvider clientId={GOOGLE_CLIENT_ID}>
